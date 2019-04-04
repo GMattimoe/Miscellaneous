0ec22dfa67bcf7ab4ccbc0a878c4249e483c02b1
GMattimoe <georgemattimoe208@gmail.com>
2019-04-04 16:38:44 +0100

    add python hangman manager

---
+++ Augmented_Hangman/hangman_python/HangmanManager.py
@@ -0,0 +1,54 @@
+#This class can be used to in a hangman game that doesnt pick a word until there
+#is no other word within an inputted dictionary that it could be. Hence "Evil Hangman"
+
+
+class HangmanManager:
+
+    def __init__(self, dictionary, length, max):
+        """Pre: Input a Collection of Strings which will be the available words, an integer
+        "length" for the length of word desired, and a integer "max" for the maximum
+        number of guesses allowed. If the maximum number of guesses is less than 0
+        or the length of the word is less that 1 throws IllegalArgumentException
+        Post:Stores the inputs appropriately and eleminates all the words from the
+        dictionary that are not of the length inputed."""
+
+        if max<0 or length <1:
+            raise ValueError("guesses must be > 0 and word must be at least 1 letter long")
+        else:
+            self.word_length = length
+            self.num_guesses_left = max
+            self.guesses = set()
+            sub_dictionary = set()
+            for word in dictionary:
+                if word.len()==length:
+                    sub_dictionary.add(word)
+            self.sub_dictionary = sub_dictionary
+
+    def words(self):
+        """returns the available words in the current dictionary"""
+        return self.sub_dictionary
+
+    def guesses_left(self):
+        """returns the number of guesses remaining in the game"""
+        return self.num_guesses_left
+    
+    def characters_guessed(self):
+        """returns the letters that have already been guessed"""
+        return self.guesses
+
+    def pattern(self):
+        """returns the pattern made from the available words in the dictionary
+           with of blank spaces for letters not yet guessed and letters that have
+           been guessed in their correct positions. If the dictionary is empty 
+           throws IllegalStateException"""
+        if len(self.sub_dictionary)==0:
+            raise ValueError("Dictionary is empty")
+        else:
+            return self.pattern_calculation()
+
+    
+
+    def pattern_calculation(self):
+        word = next(iter(self.sub_dictionary))
+        guess_set = self.guesses
+        return "no pattern calculation method made {} {}".format(word, guess_set)
